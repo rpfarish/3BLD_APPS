5a16ccbe96f1518907b5f1e58faf8a6dca2178d0
rpfarish <60075390+rpfarish@users.noreply.github.com>
2020-02-22 00:10:58 -0500

    Add files via upload

--- Main_mo10bo3v2.py
+++ Main_mo10bo3v2.py
@@ -1,5 +1,9 @@
 from statistics import mean, StatisticsError
+import gui_display_stats as gui
 
+# TODO add functionality to be able to dnf and record the time
+#  TODO Create a set of files every new session?
+# TODO Open and print using turtle a chart of stats
 # Opens source file for scrambles.
 scrambles = open("scrambles.txt", "r")
 # Opens mean of 3 text file to write to.
@@ -10,150 +14,149 @@ solves_txt = open("solves.txt", "a")
 
 # Highlight a func call
 # and press alt + enter it will show an option
-# to add type to docstring  dont think this is useful
+# to add type to docstring, don't think this is useful
 # Surround ctrl alt + t
 
-# Const
-num_of_solves = 3
-# Init all num var
-attempts = 0
-solves = 0
-display_count = 1
-# Init all lists
-display_list = []  # Stores strings
-solve_list = []  # Stores floats
-display_bo3 = []
-solve_bo3 = []
 
+def start():
+    num_of_solves = 3
+    attempts = 0
+    solves = 0
+    display_count = 1
+    num_of_mean = 0
 
-# TODO Does ip num have to be an int or can it only be a float (minus dnfs)
-# Well it can be but then you can't check for it being a dnf?
-def is_float(str1):
-    """Returns True if str1 is flr Specifically an int with one period"""
-    isfloat = False
-    if str1.count(".") == 1 or str1.replace(".", "").isdigit():
-        isfloat = True
-    return isfloat
+    display_list = []  # Stores strings
+    solve_list = []  # Stores floats
+    display_bo3 = []
+    solve_bo3 = []
 
+    def is_float(str1):
+        """Returns True if str1 is flr Specifically an int with one period"""
+        isfloat = False
+        if str1.count(".") == 1 or str1.replace(".", "").isdigit():
+            isfloat = True
+        return isfloat
 
-def ip_num(str2):
-    """Casts str2 as a float if it can be a float or is decimal"""
-    if is_float(str2) or str2.isdecimal():
-        str2_float = float(str2)
-        return str2_float
+    def is_num(str3):
+        """Returns True if the input str con be a float or if it is decimal
+        (a num that is not a float or is alphanumeric text or is complex)"""
+        is_number = False
+        if is_float(str3) or str3.isdecimal():
+            is_number = True
+        return is_number
 
+    def ip_lower_str(str4):
+        """ Function returns the string.lower if the input is a string
+        Input can be a float(if type is a str)."""
+        if isinstance(str4, str):
+            lower_case = str4.lower()
+            return lower_case
 
-def is_num(str3):
-    """Returns True if the input str con be a float or if it is decimal
-    (a num that is not a float or is alphanumeric text or is complex)"""
-    is_number = False
-    if is_float(str3) or str3.isdecimal():
-        is_number = True
-    return is_number
+    def while_ip(str_solve, disp_count=1):
+        """Inputs string solve and display count. If the input is neither
+        a 'num' nor the string 'dnf', it continues to loop."""
+        low = ip_lower_str(str_solve)
+        new_ip_solve = ""
+        if not is_num(str_solve) and not low == "dnf":
+            while not is_num(new_ip_solve) and not low == "dnf":
+                new_ip_solve = input(f"Enter solve {disp_count}: ")
+                low = ip_lower_str(new_ip_solve)
+        return low
 
+    def display_mean(solves_lst):
+        """Averages all non-dnf solves"""
+        try:
+            flt = [float(i) for i in solves_lst]
+            return f'Average: {mean(flt)} for {attempts} solves yay'
+        except StatisticsError:
+            print("StatisticsError:", solves_lst)
+        except TypeError:
+            print("TypeError: can't convert type 'str' to numerator/denominator")
 
-def ip_lower_str(str4):
-    """ Function returns the string.lower if the input is a string
-    Input can be a float(if type is a str)."""
-    if isinstance(str4, str):
-        lower_case = str4.lower()
-        return lower_case
+    # TODO Maybe add a good note func (Called analyze?)
 
-
-def append_float(num):
-    """Appends both the display list (cont DNFs) and
-        solve list (does not cont DNFs)"""
-    solve_list.append(num)
-    display_list.append(num)
-
-
-def while_ip(str_solve, dispcount=1):
-    """Inputs string solve and display count. If the input is neither
-    a 'num' nor the string 'dnf', it continues to loop."""
-    low = ip_lower_str(str_solve)
-    new_ip_solve = ""
-    if not is_num(str_solve) and not low == "dnf":
-        while not is_num(new_ip_solve) and not low == "dnf":
-            new_ip_solve = input(f"Enter solve {dispcount}: ")
-            low = ip_lower_str(new_ip_solve)
-    return low
-
-
-# display_count += 1212
-# print(while_ip("", display_count))
-
-
-def display_mean(solves_lst):
-    # TODO Figure out bo3 means and if total ave of non-dnfs is good
-    # TODO doesn't filter out dnfs
-    try:
-        flt = [float(i) for i in solves_lst]
-        print(f'Average: {mean(flt)} for {attempts} solves yay')
-    except StatisticsError:
-        print("StatisticsError:", solves_lst)
-    except TypeError:
-        print("TypeError: can't convert type 'str' to numerator/denominator")
-
-
-# TODO Print means bo3 with the mean number
-# TODO Print the Accuracy of the Bo3s, total solves and the Mo3s
-# TODO Print the Accuracy
-# TODO Print the num of successes af each mo3 after it
-# TODO Print the means of bo3s and of mo3s
-# TODO Maybe add a good note func (Called analyze?)
-
-
-while attempts < num_of_solves:
-    print("\n")
-    print("Scramble {}".format(display_count))
-    print(scrambles.readline() + "Enter solve")
-    string_solve = (input(">"))
-    lower = while_ip(string_solve)
-    if lower == "dnf":
-        lower = lower.upper()
-        display_list.append(lower)
-    else:
-        solves += 1
-        lower = float(lower)
-        display_list.append(lower)
-        solve_list.append(lower)
-    print("{} display list".format(display_list))
-    # print("{} solve list".format(solve_list))
-
-    # todo Print the means of mo3
-    if display_count % 3 == 0:
-        # TODO write the num if the mean with the stats
-        mo3.write("\n")
-        # Check if [-3:] in display_list contains a DNF
-        # Display list is still a 'str' so it should be pretty easy to check
-        dnf_in_last_3 = "DNF" in display_list[-3:]
-        if dnf_in_last_3:
-            lst_mo3 = "Last Mean of 3: DNF \n"
-            print(lst_mo3)
-            mo3.write(lst_mo3)
+    while attempts < num_of_solves:
+        print("Scramble {}".format(display_count))
+        print(scrambles.readline() + "Enter solve")
+        string_solve = (input(">"))
+        lower = while_ip(string_solve, display_count)
+        if lower == "dnf":
+            lower = lower.upper()
+            display_list.append(lower)
         else:
-            print("Last Mean of 3: " + str(mean(solve_list[-3:])) + "\n")
-            mo3.write("Last Mean of 3: " + str(mean(solve_list[-3:])) + "\n")
-        last_3_dnf_num = display_list[-3:].count("DNF")
+            solves += 1
+            lower = float(lower)
+            solve_list.append(lower)
+            display_list.append(lower)
+        print("{} display list".format(display_list[-12:]))
 
-        # TODO print bo3 and add to a bo3 list
-        if last_3_dnf_num == 3:
-            display_bo3.append("DNF")
-            print("Last Bo3: DNF" + "\n")
-        else:
-            minimum = min(solve_list[-3:])
-            display_bo3.append(minimum)
-            lst_bo3 = "Last Bo3: " + str(display_bo3[-1:]) + "\n"
-            print(lst_bo3)
-            mo3.write(lst_bo3)
+        if display_count % 3 == 0:
+            num_of_mean += 1
+            mo3.write("\n")
+            mo3.write(f"Mean of 3 #{num_of_mean}:\n")
+            mo3.write(f"{display_list[-3:]}\n")
+            # Check if [-3:] in display_list contains a DNF
+            # Display list is still a 'str' so it should be pretty easy to check
+            dnf_in_last_3 = "DNF" in display_list[-3:]
+            if dnf_in_last_3:
+                lst_mo3 = "Last Mean of 3: DNF \n"
+                print(lst_mo3)
+                mo3.write(lst_mo3)
+            else:
+                print("Last Mean of 3: " + str(mean(solve_list[-3:])) + "\n")
+                mo3.write("Last Mean of 3: " + str(mean(solve_list[-3:])) + "\n")
+            last_3_dnf_num = display_list[-3:].count("DNF")
+            # TODO reduce into a function
+            if last_3_dnf_num == 3:
+                display_bo3.append("DNF")
+                last_bo3 = "Last Bo3: DNF" + "\n"
+                print(last_bo3)
+                mo3.write(last_bo3)
+            elif last_3_dnf_num == 0:
+                minimum = min(solve_list[-3:])
+                display_bo3.append(minimum)
+                solve_bo3.append(minimum)
+                lst_bo3 = "Last Bo3: " + str(display_bo3[-1:]) + "\n"
+                print(lst_bo3)
+                mo3.write(lst_bo3)
+            elif last_3_dnf_num == 1:
+                minimum = min(solve_list[-2:])
+                display_bo3.append(minimum)
+                solve_bo3.append(minimum)
+                lst_bo3 = "Last Bo3: " + str(display_bo3[-1:]) + "\n"
+                print(lst_bo3)
+                mo3.write(lst_bo3)
+            elif last_3_dnf_num == 2:
+                minimum = min(solve_list[-1:])
+                display_bo3.append(minimum)
+                solve_bo3.append(minimum)
+                lst_bo3 = "Last Bo3: " + str(display_bo3[-1:]) + "\n"
+                print(lst_bo3)
+                mo3.write(lst_bo3)
+        print("\n")
+        display_count += 1
+        attempts += 1
 
-    display_count += 1
-    attempts += 1
-# TODO print on one line
-accuracy = "Accuracy: {}/{} \n".format(solves, num_of_solves)
-percent = "Percent {}\n".format((solves/num_of_solves)*100)
-solves_txt.write(accuracy)
-solves_txt.write(percent)
+    accuracy = "Accuracy: {}/{} \n".format(solves, num_of_solves)
+    # TODO
+    percent = "Percent {}\n".format((solves / num_of_solves) * 100)
+    mean_of_all_solves = display_mean(solve_list)
+    mo3.write(mean_of_all_solves)
+    solves_txt.write(accuracy)
+    solves_txt.write(percent)
+
+    solves_txt.write(f"Mean of Bo3: {mean(solve_bo3)}\n\n")
+    return solve_bo3
+
+bo3 = start()
+
+# This is the end of the file
+gui.window()
+gui.draw_grid(600, 300, -300, -25)
+gui.draw_num(bo3 , y=-100)
+gui.keep_open()
+
+
 # def mutate_string(string, position, character):
 # string = string[:position] + character + string[position + 1:]
 #     return string
@@ -187,5 +190,6 @@ def add_two_a_and_b(a, b):
 
 
 ax, ay = add_two_a_and_b(4, 6)
+
 # print(ax)
 # print(ay)
---
+++ gui_display_stats.py
@@ -0,0 +1,69 @@
+import turtle
+
+
+def window():
+    wn = turtle.Screen()
+    wn.title("I love you Mom. <3")
+    wn.bgcolor("grey")
+    wn.setup(width=800, height=600)
+
+
+def draw_num(string_ip, x=0, y=0, color="white"):
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color(color)
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(x, y)
+    pen.write(string_ip, align="center", font=("Comic Sans", 24, "normal"))
+
+
+def draw_square(side_length_x, side_length_y, x=0, y=0, color="black"):
+    square = turtle.Turtle()
+    square.penup()
+    square.speed(0)
+    square.color(color)
+    square.hideturtle()
+    square.goto(x, y)
+    square.shape("turtle")
+    square.pendown()
+    square.forward(side_length_x)
+    square.left(90)
+    square.forward(side_length_y)
+    square.left(90)
+    square.forward(side_length_x)
+    square.left(90)
+    square.forward(side_length_y)
+    square.right(90)
+
+
+def draw_row(forward_x, x=0, y=0, color="black"):
+    row = turtle.Turtle()
+    row.penup()
+    row.speed(0)
+    row.color(color)
+    row.hideturtle()
+    row.goto(x, y)
+    row.shape("turtle")
+    row.pendown()
+    row.forward(forward_x)
+    row.left(0)
+    row.forward(forward_x)
+
+
+def draw_rows(forward_x, x=0, y=0, color="black"):
+    displacement = 300
+    loop_len = 0
+    while loop_len < 10:
+        displacement -= 30
+        draw_row(forward_x / 2, x, y + displacement, color)
+        loop_len += 1
+        print(forward_x)
+
+
+def draw_grid(side_length_x, side_length_y, x=0, y=0, color="black"):
+    draw_square(side_length_x, side_length_y, x, y, color)
+    draw_rows(side_length_x, x, y, color)
+
+def keep_open():
+    turtle.mainloop()
